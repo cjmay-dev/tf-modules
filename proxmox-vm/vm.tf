acb7b0bb18ca54769c700d271565bf1f40c6536b
C.J. May <lawndoc@protonmail.com>
2025-05-27 22:10:24 -0500

    use noble instead of jammy

--- proxmox-vm/vm.tf
+++ proxmox-vm/vm.tf
@@ -45,7 +45,7 @@ resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
   content_type = "iso"
   datastore_id = "local"
   node_name    = "pve"
-  url = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+  url = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
   overwrite    = true
   overwrite_unmanaged = true
 }
