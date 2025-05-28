9e3aba85118c78591e5a4ffcaa976a1d6d081c5d
C.J. May <lawndoc@protonmail.com>
2025-05-27 21:05:00 -0500

    overwrite existing files

--- proxmox-vm/cloud-config.tf
+++ proxmox-vm/cloud-config.tf
@@ -2,6 +2,7 @@ resource "proxmox_virtual_environment_file" "user_data_cloud_config" {
   content_type = "snippets"
   datastore_id = "local"
   node_name    = "pve"
+  overwrite    = true
 
   source_raw {
     data = <<-EOF
