4ec5960161c90710c43bbaab0cc9abcf77a56ff4
C.J. May <lawndoc@protonmail.com>
2025-06-26 20:05:32 -0500

    persist data volume across tf changes

--- proxmox-vm/vm.tf
+++ proxmox-vm/vm.tf
@@ -1,4 +1,23 @@
+resource "proxmox_virtual_environment_volume" "data_vm" {
+  lifecycle {
+    prevent_destroy = true
+  }
+  started = false
+  on_boot = false
+
+  disk {
+    datastore_id = var.DATASTORE_ID
+    interface    = "scsi0"
+    size         = var.DISK_SIZE
+    discard      = "on"
+    ssd          = true
+  }
+}
+
 resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
+  lifecycle {
+    create_before_destroy = true
+  }
   name      = var.APP_SHORTNAME
   tags      = ["terraform", "docker"]
   node_name = "pve"
@@ -17,9 +36,21 @@ resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
     datastore_id = var.DATASTORE_ID
     file_id      = proxmox_virtual_environment_download_file.ubuntu_cloud_image.id
     interface    = "scsi0"
-    size         = var.DISK_SIZE
+    size         = 32
     discard      = "on"
     ssd          = true
+  }
+  dynamic "disk" {
+    for_each = { for idx, val in proxmox_virtual_environment_vm.data_vm.disk : idx => val }
+    iterator = data_disk
+    content {
+      datastore_id      = data_disk.value["datastore_id"]
+      path_in_datastore = data_disk.value["path_in_datastore"]
+      file_format       = data_disk.value["file_format"]
+      size              = data_disk.value["size"]
+      # assign from scsi2 and up
+      interface         = "scsi${data_disk.key + 2}"
+    }
   }
   initialization {
     ip_config {
