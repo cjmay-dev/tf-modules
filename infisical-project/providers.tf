fd714e7ccb7c11dbd411f01112c6a5e48aa922cc
C.J. May <lawndoc@protonmail.com>
2025-06-26 23:03:02 -0500

    remove provider comments

--- infisical-project/providers.tf
+++ infisical-project/providers.tf
@@ -8,15 +8,7 @@ terraform {
 }
 
 provider "infisical" {
-  # requires environment variable "INFISICAL_HOST"
   auth = {
-    oidc = {
-      # requires environment variable "INFISICAL_MACHINE_IDENTITY_ID"
-      # requires environment variable "INFISICAL_AUTH_JWT"
-    }
-    # universal = {
-    #   # requires environment variable "INFISICAL_UNIVERSAL_AUTH_CLIENT_ID"
-    #   # requires environment variable "INFISICAL_UNIVERSAL_AUTH_CLIENT_SECRET"
-    # }
+    oidc = {}
   }
 }
